f0cab122c4cad94614db2e0f03e2197f99d870e1
Ibragim <ahtubcex@ya.ru>
2018-11-17 16:54:20 +0300

    trying reminder controller

--- projectFiles/rosbank_techmadness/rosbank_techmadness/LoginViewController.swift
+++ projectFiles/rosbank_techmadness/rosbank_techmadness/LoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    var entryNum: Int = 0
     
     @IBOutlet weak var EntryNav: UINavigationItem!
     @IBOutlet weak var loginTextField: UITextField!
@@ -18,9 +19,12 @@ class LoginViewController: UIViewController {
         
         if checkStatus(){
             self.performSegue(withIdentifier: "entrySegue", sender: nil)
+            entryNum = entryNum + 1
+            reminder()
+            
         } else{
             DispatchQueue.main.async {
-                let alertController = UIAlertController(title: "Ошибка при вводе логина/пароля!", message: "", preferredStyle: UIAlertController.Style.alert)
+                let alertController = UIAlertController(title: "Не введено поле логин/пароль", message: "", preferredStyle: UIAlertController.Style.alert)
                 alertController.addAction(UIAlertAction(title: "Ок", style: UIAlertAction.Style.default,handler: nil))
                 self.present(alertController, animated: true, completion: nil)
             }
@@ -48,6 +52,35 @@ class LoginViewController: UIViewController {
         else{
             return false
         }
+    }
+    
+    func reminder()->Void{
+        if (Float(entryNum) / 1 == 1.0){
+            entryNum = 0
+            DispatchQueue.main.async {
+//                let alertController = UIAlertController(title: "Хотите вспонить ассоциацию?", message: "", preferredStyle: UIAlertController.Style.alert)
+//                alertController.addAction(UIAlertAction(title: "Да", style: UIAlertAction.Style.default,handler: nil))
+////                 alertController.addAction(UIAlertAction(title: "Позднее", style: UIAlertAction.Style.default,handler: nil))
+//                self.present(alertController, animated: true, completion: nil)
+                
+                let alert = UIAlertController(title: "Success", message: "You are validated!", preferredStyle: UIAlertController.Style.alert)
+                let defaultAction = UIAlertAction(title: "Да", style: .default, handler: {action in
+                    self.performSegue(withIdentifier: "reminderSegue", sender: nil);
+                })
+                alert.addAction(defaultAction)
+                alert.addAction(UIAlertAction(title: "Позднее", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                
+//                let defaultAction = UIAlertAction(title: "Да", style: .default, handler: {action in
+//                    self.performSegue(withIdentifier: "reminderSegue", sender: nil);
+//                })
+//                alertController.addAction(defaultAction)
+//                self.present(alertController, animated: true, completion: nil)
+                
+            }
+            
+        }
+        
     }
     
     override func viewDidLoad() {
