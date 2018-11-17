91ad1c2029d91658f2a7b18bb04cab68485923bf
Yury Shashkin <yshashkin@yandex.ru>
2018-11-17 13:39:41 +0300

    Adding ForgottenPassword

--- projectFiles/rosbank_techmadness/rosbank_techmadness/ForgottenPasswordViewController.swift
+++ projectFiles/rosbank_techmadness/rosbank_techmadness/ForgottenPasswordViewController.swift
@@ -1,21 +1,19 @@
 //
-//  LoginViewCintroller.swift
+//  ForgottenPasswordViewController.swift
 //  rosbank_techmadness
 //
 //  Created by Юрий Шашкин on 17/11/2018.
 //  Copyright © 2018 Ибрагим Мамадаев. All rights reserved.
 //
 
+
 import UIKit
 
-class LoginViewController: UIViewController {
-    
-    @IBOutlet weak var EntryNav: UINavigationItem!
+class ForgottenPasswordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.tintColor = UIColor.red
     }
-
+    
 }
 
