9fad4cf0d5655b9110746cc3ffdf82928a9e1505
Yury Shashkin <yshashkin@yandex.ru>
2018-11-17 14:53:14 +0300

    Editing FoggottenPassword

--- projectFiles/rosbank_techmadness/rosbank_techmadness/ForgottenPasswordViewController.swift
+++ projectFiles/rosbank_techmadness/rosbank_techmadness/ForgottenPasswordViewController.swift
@@ -11,8 +11,29 @@ import UIKit
 
 class ForgottenPasswordViewController: UIViewController {
     
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    @IBAction func ValueChanged(_ sender: UISegmentedControl) {
+        switch segmentControl.selectedSegmentIndex{
+        case 0:
+            myLabel.text = "Женя пидр"
+        case 1:
+            myLabel.text = "Женя очень пидр"
+        case 2:
+            myLabel.text = "Женя очень очень пидр"
+        default:
+            break
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
 }
